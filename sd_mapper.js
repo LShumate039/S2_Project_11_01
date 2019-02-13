6e84124dc712ae8641a766b3d5de5a74acf38609
Unknown <LShumate039@west-mec.org>
2019-02-12 17:19:47 -0700

    Final Product
    ready to be graded

---
+++ sd_mapper.js
@@ -0,0 +1,24 @@
+"use strict"
+
+/*
+   New Perspectives on HTML5 and CSS3, 7th Edition
+   Tutorial 9
+   Case Problem 1
+
+   Planisphere Script
+   Author: 
+   Date:   
+
+*/
+// this changes the date on the website 
+var thisTime = new Date(" February 3, 2018 03:15:28");
+var timeStr = thisTime.toLocaleString();
+document.getElementById("timeStamp").innerHTML = timeStr;
+
+var thisHour = thisTime.getHours();
+var thisMonth = thisTime.getMonth();
+
+// makes the image appear on the website 
+var mapNum = (2 * thisMonth + thisHour) % 24;
+var imgStr = "<img src='sd_sky" + mapNum + ".png' />";
+document.getElementById("planisphere").insertAdjacentHTML('afterbegin', imgStr)
